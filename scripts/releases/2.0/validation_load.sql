295141ca6a71dfdd506a2296d5ea82611fe3d120
Amy Smith <asmith@liquibase.com>
2023-03-08 10:28:31 -0600

    Adding files

--- scripts/releases/2.0/validation_load.sql
+++ scripts/releases/2.0/validation_load.sql
@@ -12,3 +12,15 @@ CREATE OR REPLACE TABLE ORDERS (
 DATA_RETENTION_TIME_IN_DAYS=2; 
 --rollback DROP TABLE ORDERS;
  
+--changeset AmySmith:orders_stage_copy labels:JIRA-5234
+CREATE OR REPLACE STAGE aws_stage_copy
+    url='s3://snowflakebucket-copyoption/size/'; 
+LIST @aws_stage_copy;
+--rollback DROP STAGE aws_stage_copy;
+
+--changeset AmySmith:orders_table_load labels:JIRA-5234
+	COPY INTO ORDERS
+    FROM @aws_stage_copy
+    file_format= (type = csv field_delimiter=',' skip_header=1)
+    pattern='.*Order.*';
+--rollback DELETE FROM ORDERS
