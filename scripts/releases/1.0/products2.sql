22e207f5138d910b344fb386fe03c93af7126117
Amy Smith <asmith@liquibase.com>
2023-03-08 15:43:04 -0600

    Fixing data retention

--- scripts/releases/1.0/products2.sql
+++ scripts/releases/1.0/products2.sql
@@ -6,6 +6,6 @@ create table products2 (
 	product_name varchar (255), 
 	contains_tracking_id boolean default True, 
 	date_first_sold date default null 
-);
---DATA_RETENTION_TIME_IN_DAYS=2;
+)
+DATA_RETENTION_TIME_IN_DAYS=2;
 --rollback DROP TABLE products
