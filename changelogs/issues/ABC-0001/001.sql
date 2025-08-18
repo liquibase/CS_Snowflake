5c7f51159f7c2cc49bcd4830c4118ddaeb123f5c
Amy Smith <asmith@liquibase.com>
2025-08-18 15:43:50 -0500

    Clean up changes

---
+++ changelogs/issues/ABC-0001/001.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset AmySmith:customers_table
+CREATE OR REPLACE TABLE CUSTOMERS (
+  "ID" INT,
+  "first_name" VARCHAR,
+  "last_name" VARCHAR,
+  "email" VARCHAR,
+  "age" INT,
+  "city" VARCHAR)
+DATA_RETENTION_TIME_IN_DAYS=2; 
+--rollback DROP TABLE CUSTOMERS
