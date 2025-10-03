73511196fa95e4a8f3bd1fd0ab71ed5508ff0980
Amy Smith <asmith@liquibase.com>
2025-10-03 12:36:36 -0500

    customers 3

--- changelogs/issues/ABC-0001/001.sql
+++ changelogs/issues/ABC-0001/001.sql
@@ -21,3 +21,14 @@ CREATE OR REPLACE TABLE CUSTOMERS2 (
   "city" VARCHAR)
 DATA_RETENTION_TIME_IN_DAYS=2; 
 --rollback DROP TABLE CUSTOMERS2
+
+--changeset AmySmith:customers3_table labels:jira-1235
+CREATE OR REPLACE TABLE CUSTOMERS3 (
+  "ID" INT,
+  "first_name" VARCHAR,
+  "last_name" VARCHAR,
+  "email" VARCHAR,
+  "age" INT,
+  "city" VARCHAR)
+DATA_RETENTION_TIME_IN_DAYS=3; 
+--rollback DROP TABLE CUSTOMERS3
